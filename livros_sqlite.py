171316f910db7d9f11c193d5387a1ed8596bed8d
larissabusquet <larissabusquet@gmail.com>
2025-11-03 17:55:43 -0300

    etapa: inseridos 5 livros fictícios na tabela

--- livros_sqlite.py
+++ livros_sqlite.py
@@ -21,6 +21,32 @@ cursor.execute('''
 conn.commit()
 print("Tabela Livros criada")
 
+# ETAPA 4: Inserir 5 livros fictícios
+print("\n[ETAPA 4] Inserindo 5 livros fictícios...")
+
+livros = [
+    ("1984", "George Orwell", 1949, "Ficção Distópica", 1),
+    ("O Senhor dos Anéis", "J.R.R. Tolkien", 1954, "Fantasia", 1),
+    ("Dom Casmurro", "Machado de Assis", 1899, "Romance", 1),
+    ("O Pequeno Príncipe", "Antoine de Saint-Exupéry", 1943, "Infantil", 0),
+    ("A Revolução dos Bichos", "George Orwell", 1945, "Fábula Política", 1)
+]
+
+# Inserindo os livros no banco de dados
+cursor.executemany('''
+    INSERT OR IGNORE INTO Livros (titulo, autor, ano, genero, disponivel)
+    VALUES (?, ?, ?, ?, ?)
+''', livros)    
+
+conn.commit()
+print("livros inseridos com sucesso") 
+
+#mostrar os livros inseridos
+cursor.execute("SELECT * FROM Livros")
+print("Livros cadastrados:")
+for livro in cursor.fetchall():
+    print(f"ID: {livro[0]}, Título: {livro[1]}, Autor: {livro[2]}, Ano: {livro[3]}, Gênero: {livro[4]}, Disponível: {'Sim' if livro[5] == 1 else 'Não'}")
+    
 # Fechando a conexão
 conn.close()
 print("Conexão encerrada")
